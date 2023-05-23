95474077c9345ffad8458e2704ca7fc44775b33e
unknown <belen.septian@mdp.ac.id>
2023-05-23 12:43:19 +0700

    add soure code

---
+++ source code/test_camera.py
@@ -0,0 +1,17 @@
+#Import Library OpenCV
+import cv2
+
+#Variable untuk VideoCapture
+cap = cv2.VideoCapture(1)
+
+#Fungsi untuk membuat frame pengaturan pada video
+while(True):
+    #Membaca video
+    ret, frame = cap.read()
+    #Menampilkan video
+    cv2.imshow('frame',frame)
+    #Pengaturan frame
+    if cv2.waitKey(1) == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
