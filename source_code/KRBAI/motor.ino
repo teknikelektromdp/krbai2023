f1c7d131b02395d89a1755d20cf80670457432e8
ali zainal <ali@mdp.ac.id>
2023-06-14 14:36:15 +0700

    update coding utama

--- source_code/KRBAI/motor.ino
+++ source_code/KRBAI/motor.ino
@@ -18,66 +18,102 @@ void motorGo(int motor, int direct, int pwm)
 
 void forward()
 {
-  motorGo(0,cw,75);
-  motorGo(1,ccw,100);
-  motorGo(2,cw,75);
-  motorGo(3,cw,100);
+//  motorGo(0,cw,75);
+//  motorGo(1,ccw,100);
+//  motorGo(2,cw,75);
+//  motorGo(3,cw,100);
+  thrus_1.writeMicroseconds(1300);
+  thrus_2.writeMicroseconds(1700);
+  thrus_3.writeMicroseconds(1300);
+  thrus_4.writeMicroseconds(1300);
 }
 void back()
 {
-  motorGo(0,ccw,75);
-  motorGo(1,cw,75);
-  motorGo(2,ccw,75);
-  motorGo(3,ccw,75);
+//  motorGo(0,ccw,75);
+//  motorGo(1,cw,75);
+//  motorGo(2,ccw,75);
+//  motorGo(3,ccw,75);
+  thrus_1.writeMicroseconds(1700);
+  thrus_2.writeMicroseconds(1300);
+  thrus_3.writeMicroseconds(1700);
+  thrus_4.writeMicroseconds(1700);
 }
 void shift_left()
 {
-  motorGo(0,ccw,75);
-  motorGo(1,ccw,75);
-  motorGo(2,cw,75);
-  motorGo(3,ccw,75);
+//  motorGo(0,ccw,75);
+//  motorGo(1,ccw,75);
+//  motorGo(2,cw,75);
+//  motorGo(3,ccw,75);
+  thrus_1.writeMicroseconds(1700);
+  thrus_2.writeMicroseconds(1700);
+  thrus_3.writeMicroseconds(1300);
+  thrus_4.writeMicroseconds(1700);
 }
 void shift_right()
 {
-  motorGo(0,cw,75);
-  motorGo(1,cw,75);
-  motorGo(2,ccw,75);
-  motorGo(3,cw,75);
+//  motorGo(0,cw,75);
+//  motorGo(1,cw,75);
+//  motorGo(2,ccw,75);
+//  motorGo(3,cw,75);
+  thrus_1.writeMicroseconds(1300);
+  thrus_2.writeMicroseconds(1300);
+  thrus_3.writeMicroseconds(1700);
+  thrus_4.writeMicroseconds(1300);
 }
 void right_rotate()
 {
-  motorGo(0,cw,75);
-  motorGo(1,cw,75);
-  motorGo(2,cw,75);
-  motorGo(3,ccw,75);
+//  motorGo(0,cw,75);
+//  motorGo(1,cw,75);
+//  motorGo(2,cw,75);
+//  motorGo(3,ccw,75);
+  thrus_1.writeMicroseconds(1300);
+  thrus_2.writeMicroseconds(1300);
+  thrus_3.writeMicroseconds(1300);
+  thrus_4.writeMicroseconds(1700);
 }
 void rr_PID(int kec)
 {
-  motorGo(0,cw,kec);
-  motorGo(1,cw,kec);
-  motorGo(2,cw,kec);
-  motorGo(3,ccw,kec);
+//  motorGo(0,cw,kec);
+//  motorGo(1,cw,kec);
+//  motorGo(2,cw,kec);
+//  motorGo(3,ccw,kec);
+  thrus_1.writeMicroseconds(1500 - kec);
+  thrus_2.writeMicroseconds(1500 - kec);
+  thrus_3.writeMicroseconds(1500 - kec);
+  thrus_4.writeMicroseconds(1500 + kec);
 }
 void left_rotate()
 {
-  motorGo(0,ccw,75);
-  motorGo(1,ccw,75);
-  motorGo(2,ccw,75);
-  motorGo(3,cw,75);
+//  motorGo(0,ccw,75);
+//  motorGo(1,ccw,75);
+//  motorGo(2,ccw,75);
+//  motorGo(3,cw,75);
+  thrus_1.writeMicroseconds(1700);
+  thrus_2.writeMicroseconds(1700);
+  thrus_3.writeMicroseconds(1700);
+  thrus_4.writeMicroseconds(1300);
 }
 void lr_PID(int kec)
 {
-  motorGo(0,ccw,kec);
-  motorGo(1,ccw,kec);
-  motorGo(2,ccw,kec);
-  motorGo(3,cw,kec);
+//  motorGo(0,ccw,kec);
+//  motorGo(1,ccw,kec);
+//  motorGo(2,ccw,kec);
+//  motorGo(3,cw,kec);
+  thrus_1.writeMicroseconds(1500 + kec);
+  thrus_2.writeMicroseconds(1500 + kec);
+  thrus_3.writeMicroseconds(1500 + kec);
+  thrus_4.writeMicroseconds(1500 - kec);
 }
 void berhenti()
 {
-  motorGo(0,cw,0);
-  motorGo(1,cw,0);
-  motorGo(2,cw,0);
-  motorGo(3,cw,0);
+//  motorGo(0,cw,0);
+//  motorGo(1,cw,0);
+//  motorGo(2,cw,0);
+//  motorGo(3,cw,0);
+  thrus_1.writeMicroseconds(1500);
+  thrus_2.writeMicroseconds(1500);
+  thrus_3.writeMicroseconds(1500);
+  thrus_4.writeMicroseconds(1500);
 }
 void SOS()
 {
