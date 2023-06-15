c1ca79b383f0da88739c6e1ad8f106b751b6d24e
ali zainal <ali@mdp.ac.id>
2023-06-15 14:26:45 +0700

    update coding utama

--- source_code/KRBAI/Serial_data.ino
+++ source_code/KRBAI/Serial_data.ino
@@ -1,12 +1,12 @@
 void tampil(){
-  Serial.print("roll: ");          Serial.print(roll);
-  Serial.print(", pitch: ");       Serial.print(pitch);
+//  Serial.print("roll: ");          Serial.print(roll);
+//  Serial.print(", pitch: ");       Serial.print(pitch);
   Serial.print(", Yaw(angle): ");  Serial.println(yaw);
 
-  Serial.print("SP_depth(cm): ");    Serial.print(set_level);
+//  Serial.print("SP_depth(cm): ");    Serial.print(set_level);
 //  Serial.print(", set_point: ");        Serial.print(de_Setpoint);
-  Serial.print(", Pressure: ");   Serial.print(avg);
-  Serial.print(", water_lvl: ");  Serial.println(water_level);
+//  Serial.print(", Pressure: ");   Serial.print(avg);
+//  Serial.print(", water_lvl: ");  Serial.println(water_level);
 //  Serial.println(de_Output);
   delay(100);
 }
